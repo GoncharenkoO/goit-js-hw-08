d88a4240c0532c1cd93f975c874cc66aa3b7e358
Olga Goncharenko <goncharenko86@gmail.com>
2022-01-18 12:34:46 +0200

    Зміни

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,17 +4,16 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-
 const onPlay = function (event) {
     localStorage.setItem("videoplayer-current-time", event.seconds);
     };
 
     player.on('timeupdate', throttle(onPlay, 1000));
     
-
-    if (!localStorage.getItem('videoplayer-current-time')) {
-        return;
-    } else {
+    if (localStorage.getItem('videoplayer-current-time')) {
         const currenTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
     player.setCurrentTime(currenTime);
-}
+
+    }
+
+  
